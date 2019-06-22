046ec546b5b59d1a2cc1b812a21424d64b8f966b
root <root@localhost.localdomain>
2019-06-22 17:17:36 +0800

    update vimrc

---
+++ DataStruture/dynamic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int rec_opt(int arr[], int i){
+	if ( i == 0 ) {
+		return arr[0];
+	} else if ( i == 1 ){
+		if ( a==0 ) {
+			a = ;
+			if (i %= 3) {
+			}
+		}
+	}
+}
+
+void main(){
+
+}
